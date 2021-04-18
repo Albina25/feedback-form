9f48dd9e71aed452a1377c44603b8ccecac6e14f
Albina25 <77781085+Albina25@users.noreply.github.com>
2021-04-18 14:01:28 +0500

    Add files via upload

--- src/sendemail.php
+++ src/sendemail.php
@@ -6,14 +6,27 @@
 // $phone = $input['phone'];
 
 // $message = "Name: ".$name."\nPhone: ".$phone;
-$c = true;
+
 $name = 'Имя: '.$_POST['name'];
-$mail =  'Почта: '.$_POST['phone'];
+$phone =  'Почта: '.$_POST['phone'];
 $time =  'Время: '.$_POST['time'];
 $allInOne =  $name.$phone.$time;
-$to = 'axioma.25@gmail.com'; 
+$to = 'site@test.ru'; 
 $headers="From: Site <site@test.ru>\nContent-Type: text/html; charset=\"utf-8\"\n"; 
  
+if (!empty($phone)) {
+  if (!preg_match('/^(8|7)(\d{10})$/', $phone)) {
+    echo 'Поле Телефон содержит не корректный номер!';
+    
+  }
+}
+if (!empty($name)) {
+  if (!preg_match('/^[A-Za-zА-Яа-яЁё]*$/i', $name)) {
+    echo 'Поле Имя не корректное!';
+    
+  }
+}
+
 if (mail($to, 'Свяжитесь с нами', $AllInOne, $headers)) {
     http_response_code(200);
     echo "Данные отправлены.";
