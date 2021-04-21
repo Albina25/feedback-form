72742860a8f737393a65a10aca805d8ae4c099e0
Albina25 <77781085+Albina25@users.noreply.github.com>
2021-04-22 01:43:05 +0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 import VModal from 'vue-js-modal'
-Vue.use(VModal)
+Vue.use(VModal, {
+  dynamicDefaults: {
+    draggable: true,
+    resizable: true,
+    height: 'auto'
+  }
+})
 
 import VueSweetalert2 from 'vue-sweetalert2'
 import Swal from 'sweetalert2'
@@ -13,6 +19,9 @@ Vue.use(VueSweetalert2)
 
 import VueTheMask from 'vue-the-mask'
 Vue.use(VueTheMask)
+
+import '@/fontawesome/css/all.css'
+
 
 new Vue({
   render: h => h(App),
