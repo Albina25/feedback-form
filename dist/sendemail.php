505a16c81e315ecfc40b06ab967d95a470eb9718
Albina25 <77781085+Albina25@users.noreply.github.com>
2021-04-22 22:12:13 +0500

    Add files via upload

---
+++ dist/sendemail.php
@@ -0,0 +1,39 @@
+<?php
+ $inputJSON = file_get_contents('php://input');
+ $input = json_decode($inputJSON, TRUE);
+
+// $name = $input['name'];
+// $phone = $input['phone'];
+
+// $message = "Name: ".$name."\nPhone: ".$phone;
+
+$name = trim($_POST["name"]);
+$phone =  'Телефон: '.$_POST['phone'];
+$time =  'Время: '.$_POST['time'];
+$allInOne =  $name.$phone.$time;
+$to = 'site@test.ru'; 
+$headers="Заявка с сайта"; 
+ 
+// if (!empty($phone)) {
+//   if (!preg_match('/^(8|7)(\d{10})$/', $phone)) {
+//     echo 'Поле Телефон содержит не корректный номер!';
+    
+//   }
+// }
+// if (!empty($name)) {
+//   if (!preg_match('/^[A-Za-zА-Яа-яЁё]*$/i', $name)) {
+//     echo 'Поле Имя не корректное!';
+    
+//   }
+// }
+
+if (mail($to, 'Свяжитесь с нами', $input, $headers)) {
+    http_response_code(200);
+    echo "Данные отправлены.";
+} else {
+    http_response_code(400);
+    echo "Ошибка. Данные не отправлены.";
+  }
+
+ 
+?>
